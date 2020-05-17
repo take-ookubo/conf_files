0a4169427e599c6630acafd0bcb9e64d00792e5c
take ookubo <take.ookubo@gmail.com>
2020-05-17 20:29:35 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+function backup() {
+  if [ -f $1 ]; then
+    rm -rf "${1}.bak"
+    mv ${1} "${1}.bak"
+  fi
+}
+
+# Setup ~/.git***
+echo "Setup ~/.git***"
+backup ~/.gitconfig
+backup ~/.git-prompt.sh
+backup ~/.gitignore_global
+
+ln -s `pwd`/git/.gitconfig ~/.gitconfig
+ln -s `pwd`/git/.git-prompt.sh ~/.git-prompt.sh
+ln -s `pwd`/git/.gitignore_global ~/.gitignore_global
+
+echo "Setup ~/.bash_profile"
+backup ~/.bash_profile
+ln -s `pwd`/bash/.bash_profile ~/.bash_profile
+
+# Setup brew
+if [ ! -f /usr/local/bin/brew ]; then
+  echo "Setup brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# Setup zlib
+if [ ! -L `brew --prefix zlib` ]; then
+  echo "Setup zlib"
+  brew install zlib
+fi
+
+# Setup anyenv
+if [ ! -L `brew --prefix anyenv` ]; then
+  echo "Setup anyenv"
+  brew install anyenv
+  anyenv init
+  anyenv install --init
+  exec $SHELL -l
+fi
